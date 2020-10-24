a9cc8c1c86b02d2c8370092a7b1ea4a967194c46
Mihai Gătejescu <gus666xe@gmail.com>
2020-10-25 00:31:17 +0300

    Clean and add input test stub

--- tests.py
+++ tests.py
@@ -3,19 +3,20 @@ import unittest
 import csv2sql
 
 class SampleTest(unittest.TestCase):
+
     def test_open_file(self):
         f = csv2sql.open_file("test.csv")
         self.assertTrue(f)
         f.close()
+
     def test_parse_columns(self):
-        columns_line = "COLUMN1,COLUMN2,COLUMN3"
-        expected = ["COLUMN1", "COLUMN2", "COLUMN3"]
-        result = csv2sql.parse_columns(columns_line)
-        self.assertTrue(result == expected)
         columns_line = ",COLUMN1,COLUMN2,,COLUMN3,"
         expected = ["","COLUMN1", "COLUMN2", "", "COLUMN3", ""]
         result = csv2sql.parse_columns(columns_line)
-        self.assertTrue(result == expected)
+        self.assertEqual(result, expected)
+
+    def test_input(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
