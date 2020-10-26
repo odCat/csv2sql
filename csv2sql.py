944ffcfb2e2632a615e3efdf5f29d23643b61290
Mihai Gătejescu <gus666xe@gmail.com>
2020-10-26 21:11:49 +0200

    Add get_titles()

--- csv2sql.py
+++ csv2sql.py
@@ -1,5 +1,9 @@
-def open_file(file):
-    return open(file)
+def open_file(aFile):
+    return open(aFile)
 
 def parse_columns(columns):
     return columns.split(',')
+
+def get_titles(aFile):
+    titles = aFile.readline().rstrip()
+    return titles.split(',')
--- tests.py
+++ tests.py
@@ -5,18 +5,26 @@ import csv2sql
 class SampleTest(unittest.TestCase):
 
     def test_open_file(self):
-        f = csv2sql.open_file("test.csv")
+        f = csv2sql.open_file('test.csv')
         self.assertTrue(f)
         f.close()
 
     def test_parse_columns(self):
-        columns_line = ",COLUMN1,COLUMN2,,COLUMN3,"
-        expected = ["","COLUMN1", "COLUMN2", "", "COLUMN3", ""]
+        columns_line = ',COLUMN1,COLUMN2,,COLUMN3,'
+        expected = ['','COLUMN1', 'COLUMN2', '', 'COLUMN3', '']
         result = csv2sql.parse_columns(columns_line)
         self.assertEqual(result, expected)
 
     def test_input(self):
-        pass
+        f = open('test.csv', 'w')
+        f.write('column1,columns2,column3,column4\n')
+        f.write(',value2,value3,value4\n')
+        f.close()
+        f = open('test.csv', 'r')
+        titles = csv2sql.get_titles(f)
+        expected_titles= ['column1','columns2','column3','column4']
+        self.assertEqual(expected_titles, titles)
+        f.close()
 
 if __name__ == '__main__':
     unittest.main()
